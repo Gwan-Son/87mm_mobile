953c76bb89ec962c420381b3cb5a4619dec1073f
GwanSon <id147369@naver.com>
2024-11-28 18:02:18 +0900

    feat: 컬렉션뷰 추가 중

---
+++ 87mm_mobile/ProductCell.swift
@@ -0,0 +1,21 @@
+//
+//  ProductCell.swift
+//  87mm_mobile
+//
+//  Created by 심관혁 on 11/28/24.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    static let identifier = "ProductCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ 87mm_mobile/ProductCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCollectionView.swift
+//  87mm_mobile
+//
+//  Created by 심관혁 on 11/28/24.
+//
+
+import UIKit
+
+class ProductCollectionView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
