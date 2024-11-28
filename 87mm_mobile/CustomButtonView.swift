9894b12be3cd3a807fb743f5cfc2024ee0ce597b
GwanSon <id147369@naver.com>
2024-11-28 18:02:06 +0900

    feat: 입력되는 String값에 따라 시스템 이미지인지 에셋 이미지인지 구별하는 커스텀버튼 뷰

--- 87mm_mobile/CustomButtonView.swift
+++ 87mm_mobile/CustomButtonView.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class CustomButtonView: UIView {
+class CustomButtonView: UIButton {
+    var imageName: String? {
+        didSet {
+            updateButtonImage()
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .main
-        layer.cornerRadius = 10
+        setupButton()
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupButton() {
+        backgroundColor = .clear
+        layer.masksToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func updateButtonImage() {
+        guard let imageName = imageName else {
+            setImage(nil, for: .normal)
+            return
+        }
+        
+        if let image = UIImage(systemName: imageName) {
+            setImage(image, for: .normal)
+        } else if let image = UIImage(named: imageName) {
+            let resizedImage = image.resizeImageTo(size: CGSize(width: 44, height: 28))
+            setImage(resizedImage, for: .normal)
+        } else {
+            setImage(nil, for: .normal)
+        }
     }
 }
