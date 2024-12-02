3308681db37ddf300d9b330281d1d803f30800f4
GwanSon <id147369@naver.com>
2024-12-02 15:59:30 +0900

    feat: 더미데이터 추가

---
+++ 87mm_mobile/ProductData.swift
@@ -0,0 +1,39 @@
+//
+//  ProductData.swift
+//  87mm_mobile
+//
+//  Created by 심관혁 on 11/29/24.
+//
+
+import Foundation
+
+struct ProductData: Hashable {
+    let id: UUID = UUID()
+    let name: String
+    let imagename: String
+    let urlString: String
+    let price: String
+}
+
+extension ProductData {
+    static let list = [
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원"),
+        ProductData(name: "[Mmlg] TRAVEL DOWN (DENVER GREEN)", imagename: "product1", urlString: "https://m.87mm.co.kr/product/mmlg-travel-down-denver-green/4833/category/534/display/1/", price: "131,400원")
+    ]
+}
