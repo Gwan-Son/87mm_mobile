068fc50352cae116b54652cb3a60b0e4304d4125
GwanSon <id147369@naver.com>
2024-12-02 15:59:15 +0900

    feat: CollectionViewCell 구성

--- 87mm_mobile/ProductCell.swift
+++ 87mm_mobile/ProductCell.swift
@@ -11,11 +11,103 @@ class ProductCell: UICollectionViewCell {
     
     static let identifier = "ProductCell"
     
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let productLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let sizeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "S   M   L"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 14)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.alpha = 0.5
+        return view
+    }()
+    
+    
+    
+    func configure(_ data: ProductData) {
+        imageView.image = UIImage(named: data.imagename)
+        productLabel.text = data.name
+        priceLabel.text = data.price
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(productLabel)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(sizeLabel)
+        contentView.addSubview(dividerView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        productLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        sizeLabel.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+            
+            productLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            productLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            productLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            productLabel.heightAnchor.constraint(equalToConstant: 30),
+            
+            priceLabel.topAnchor.constraint(equalTo: productLabel.bottomAnchor, constant: 8),
+            priceLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            priceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            priceLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            sizeLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 8),
+            sizeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            sizeLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            sizeLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            dividerView.heightAnchor.constraint(equalToConstant: 1),
+            dividerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            dividerView.topAnchor.constraint(equalTo: sizeLabel.bottomAnchor, constant: 10)
+            
+        ])
     }
 }
